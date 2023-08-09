3b4aa7371bea8b9a3811f373995539a8df94247f
Alvin Yudha Eka P <alvinyudha73@gmail.com>
2023-08-09 21:59:02 +0700

    up

--- src/main/java/Form/FormBarang.java
+++ src/main/java/Form/FormBarang.java
@@ -32,11 +32,11 @@ public class FormBarang extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 849, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 375, Short.MAX_VALUE)
         );
 
         pack();
