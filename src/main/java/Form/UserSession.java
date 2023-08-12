10c7d1ad30acff33537fa529b0eed2c7f2074ee4
sendy452 <sendyivenyulian452@gmail.com>
2023-08-12 20:29:04 +0700

    example form login and session

---
+++ src/main/java/Form/UserSession.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Form;
+
+/**
+ *
+ * @author Sendy
+ */
+public class UserSession {
+
+    private static int stok, harga;
+    private static String nama;
+
+    public static String getNama() {
+        return nama;
+    }
+
+    public static void setNama(String nama) {
+        UserSession.nama = nama;
+    }
+
+    public static int getStok() {
+        return stok;
+    }
+
+    public static void setStok(int stok) {
+        UserSession.stok = stok;
+    }
+
+    public static int getHarga() {
+        return harga;
+    }
+
+    public static void setHarga(int harga) {
+        UserSession.harga = harga;
+    }
+}
